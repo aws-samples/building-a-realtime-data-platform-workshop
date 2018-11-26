5c5f97f61f0af7ede379eeeec71999634a76ced3
Injae Kwak <injakwak@amazon.com>
2018-11-26 14:14:15 +1100

    syncing with published repo

---
+++ produce.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true
+do
+	python3 ./clickstream.py
+	echo "Press [CTRL+C] to stop.."
+	sleep 1
+done
+
+
---
+++ produce_connect.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true
+do
+	python3 ./amazonconnect.py
+	echo "Press [CTRL+C] to stop.."
+	sleep 1
+done
+
+
